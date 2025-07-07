218383b3fc45ee43fd62e27732123d42ca172a78
sligas3 <sligas3@gmail.com>
2025-07-07 19:39:57 -0300

    vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
 }
 bootstrap();
+
+export default server;
