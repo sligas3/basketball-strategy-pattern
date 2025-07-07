d0a0fe6406f9d9f8e3c3e12e1d9962b9d867d36a
sligas3 <sligas3@gmail.com>
2025-07-07 20:25:54 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 // src/main.ts
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as express from "express";
 
 const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors({
+    origin: "https://basketball-strategy-ui.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
   await app.init();
 }
 bootstrap();
